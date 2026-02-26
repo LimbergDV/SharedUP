df0eee356c297f64bd7e36659043cb361a344af0
Limberg Del Valle <limbergdelvalle@gmail.com>
2026-02-26 00:20:33 -0600

    [MODIFIED]: Modifing screens to login and register. I finished

--- app/src/main/java/com/limbergdv/sharedup/SharedUPApplication.kt
+++ app/src/main/java/com/limbergdv/sharedup/SharedUPApplication.kt
@@ -1,7 +1,45 @@
 package com.limbergdv.sharedup
 
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.limbergdv.sharedup.features.authentication.presentation.screens.LoginScreen
+import com.limbergdv.sharedup.features.authentication.presentation.screens.RegisterScreen
+import com.limbergdv.sharedup.features.home.presentation.screens.HomeScreen
 
-@HiltAndroidApp
-class SharedUPApplication : Application()
+@Composable
+fun SharedUPApplication() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen(
+                onRegisterClick = { navController.navigate("register") },
+                onLoginSuccess = {
+                    navController.navigate("home") {
+                        popUpTo("login") { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable("register") {
+            RegisterScreen(
+                onLoginClick = { navController.popBackStack() },
+                onRegisterSuccess = {
+                    navController.navigate("login") {
+                        popUpTo("register") { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable("home") {
+            HomeScreen()
+        }
+    }
+}
